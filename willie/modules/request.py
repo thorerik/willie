faa0d58167558c186093d096070a333e37592a98
Thor Erik Lie <thor@thorerik.com>
2014-10-25 01:51:46 +0200

    merge request from djweb-module

---
+++ willie/modules/request.py
@@ -0,0 +1,14 @@
+from __future__ import unicode_literals
+from willie.module import commands
+import urllib2
+
+@commands('request')
+def request(bot, trigger):
+    """Request Bot"""
+#    bot.say('debug: ' + trigger.group(2))
+    opener = urllib2.build_opener(urllib2.HTTPHandler)
+    request = urllib2.Request('http://djweb.eurotruckradio.com/request.php', data='name=IRC User ' + trigger.nick + '&type=Song+Request&request=' + trigger.group(2) + '&submit=Request')
+    request.add_header('Application', 'x-www-form-urlencoded')
+    request.get_method = lambda: 'POST'
+    url = opener.open(request)
+    bot.say('Thank you ' + trigger.nick + '! I got your request for ' + (trigger.group(2) + '.'))
